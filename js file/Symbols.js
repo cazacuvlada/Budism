5559c3049b25f9f4035e8d41f818d2e30a48ba80
cazacuvlada <vlada.cazacu@iis.utm.md>
2022-04-10 20:21:05 +0300

    images

--- js file/Symbols.js
+++ js file/Symbols.js
@@ -1,3 +1,11 @@
-function myGrid() {
-    document.getElementById("hoverButton").classList.toggle("show");
-}
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'showImageNumberLabel': false,
+    'maxWidth': 500,
+    'positionFromTop': 200,
+    'data-disable-external-check':"(true)"
+
+
+})
+
